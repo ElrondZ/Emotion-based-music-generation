511e50360dcb9c81cba661913a63b4f5905466d0
gary <runfeng_gai@163.com>
2022-12-13 21:56:08 -0500

    img

--- crawl.py
+++ crawl.py
@@ -2,7 +2,10 @@ import tkinter
 import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import tkinter
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+
 
 def get_weather(weather):
     res = "sunny"
@@ -41,8 +44,17 @@ def get_index():
     return index
 
 
-# root = tkinter.Tk()
-# root.title("Main Page")
-# root.geometry("400x200")
-# label = tkinter.Label(root, text="welcome")
-# label.pack()
+root = tk.Tk()
+root.title("Main Page")
+root.geometry("400x200")
+label = tkinter.Label(root, text="welcome")
+label.pack()
+
+b1 = ttk.Button(root, text="Button 1", bootstyle=SUCCESS)
+b1.pack(side=LEFT, padx=5, pady=10)
+
+b2 = ttk.Button(root, text="Button 2", bootstyle=(INFO, OUTLINE))
+b2.pack(side=LEFT, padx=5, pady=10)
+
+root.mainloop()
+
